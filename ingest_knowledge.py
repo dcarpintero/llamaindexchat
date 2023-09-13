b09d2da636a4da45594fde463773468675527bb4
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2023-09-13 15:21:50 +0200

    refactor

--- ingest_knowledge.py
+++ ingest_knowledge.py
@@ -11,9 +11,11 @@ from llama_index import download_loader, ServiceContext, VectorStoreIndex
 from llama_index.llms import OpenAI
 from llama_hub.github_repo import GithubRepositoryReader, GithubClient
 from dotenv import load_dotenv
+import json
 import openai
 import os
 import logging
+
 
 def load_environment_vars() -> dict:
     """Load required environment variables. Raise an exception if any are missing."""
@@ -31,14 +33,12 @@ def load_environment_vars() -> dict:
     logging.info("Environment variables loaded.")
     return {"OPENAI_API_KEY": api_key, "GITHUB_TOKEN": github_token}
 
-def index_knowledge() -> VectorStoreIndex:
-    """Load and Index Knowledge Base from GitHub Repository"""
 
-    env_vars = load_environment_vars()
-    openai.api_key = env_vars['OPENAI_API_KEY']
-    
+def initialize_github_loader(github_token: str) -> GithubRepositoryReader:
+    """Initialize GithubRepositoryReader"""	
+
     download_loader("GithubRepositoryReader")
-    github_client = GithubClient(os.getenv("GITHUB_TOKEN"))
+    github_client = GithubClient(github_token)
 
     loader = GithubRepositoryReader(
         github_client,
@@ -50,40 +50,45 @@ def index_knowledge() -> VectorStoreIndex:
         concurrent_requests =    10,
     )
 
-    try:
-        logging.info("Loading data from Github: %s/%s", loader._owner, loader._repo) 
+    return loader
 
-        docs = loader.load_data(branch="main")
-        for doc in docs:
-            logging.info(doc.extra_info)
-            doc.metadata = {'filename': doc.extra_info['file_name'], 'author': "LlamaIndex"}
 
-    except Exception as e:
-        logging.error("Error loading data from Github: %s", e)
-        return None
-    
-    try:
-        logging.info("Creating ServiceContext...")
+def load_and_index_data(loader: GithubRepositoryReader) -> VectorStoreIndex:
+    """Load and Index Knowledge Base from GitHub Repository"""
+
+    logging.info("Loading data from Github: %s/%s", loader._owner, loader._repo)
+    docs = loader.load_data(branch="main")
+    for doc in docs:
+        logging.info(doc.extra_info)
+        doc.metadata = {'filename': doc.extra_info['file_name'], 'author': "LlamaIndex"}
         
-        service_context = ServiceContext.from_defaults(
-            llm=OpenAI(
-                model="gpt-3.5-turbo",
-                temperature=0.5,
-                system_prompt="You are a specialized AI trained in the usage of LlamaIndex.",
-            )
+    logging.info("Creating ServiceContext...")
+    service_context = ServiceContext.from_defaults(
+        llm=OpenAI(
+            model="gpt-3.5-turbo",
+            temperature=0.5,
+            system_prompt="You are a specialized AI trained in the usage of LlamaIndex.",
         )
+    )
 
-        logging.info("Indexing data...")
-        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+    logging.info("Indexing data...")
+    index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         
-        logging.info("Persisting index on ./storage...")
-        index.storage_context.persist(persist_dir="./storage")
+    logging.info("Persisting index on ./storage...")
+    index.storage_context.persist(persist_dir="./storage")
         
-        logging.info("Data-Knowledge ingestion process is completed (OK)")
-    except Exception as e:
-        logging.error("Error indexing or persisting data: %s", e)
-        return None
+    logging.info("Data-Knowledge ingestion process is completed (OK)")
+    return index
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
-    index_knowledge()
+
+    try:
+        env_vars = load_environment_vars()
+        openai.api_key = env_vars['OPENAI_API_KEY']
+
+        loader = initialize_github_loader(env_vars['GITHUB_TOKEN'])
+        index = load_and_index_data(loader)
+    except Exception as ex:
+        logging.error("Unexpected Error: %s", ex)
+        raise ex
